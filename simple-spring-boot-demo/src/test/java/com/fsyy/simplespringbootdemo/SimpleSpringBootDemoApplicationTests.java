6ff5018d9a620bb7d272bccf78320c127c4e108f
lianbang <1418685694@qq.com>
2024-03-21 10:42:57 +0800

    init a demo

---
+++ simple-spring-boot-demo/src/test/java/com/fsyy/simplespringbootdemo/SimpleSpringBootDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.fsyy.simplespringbootdemo;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SimpleSpringBootDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
