81dfec380826d40e978c7160bf2f12f886e25e5c
lianbang <1418685694@qq.com>
2024-03-21 14:45:23 +0800

    add a test case!

--- fsyy-web-demo/src/main/java/com/fsyy/fsyywebdemo/web/mvc/TestController.java
+++ fsyy-web-demo/src/main/java/com/fsyy/fsyywebdemo/web/mvc/TestController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Test {
+public class TestController {
     @GetMapping("/")
     public String hello(){
         throw new RuntimeException("hello world!");
--- fsyy-web-demo/src/test/java/com/fsyy/fsyywebdemo/FsyyWebDemoApplicationTests.java
+++ fsyy-web-demo/src/test/java/com/fsyy/fsyywebdemo/FsyyWebDemoApplicationTests.java
@@ -1,13 +1,50 @@
 package com.fsyy.fsyywebdemo;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@WebAppConfiguration
 public class FsyyWebDemoApplicationTests {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FsyyWebDemoApplicationTests.class);
+
+    /*@Autowired
+    private TestController testController;*/
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        // mockMvc = MockMvcBuilders.standaloneSetup(testController).build();
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
 
     @Test
-    public void contextLoads() {
+    public void contextLoads() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.status().is5xxServerError())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+
+        LOGGER.info(mvcResult.getResponse().getContentAsString());
     }
 
 }
