32d3d9827726d16756c4098ead83829abbd2d8f6
lianbang <1418685694@qq.com>
2024-03-25 21:54:34 +0800

    add a springboot3 pom, not success, need use toolchain!

---
+++ fsyy-spring-boot3/src/main/java/com/fsyy/fsyyspringboot3/FsyySpringBoot3Application.java
@@ -0,0 +1,13 @@
+package com.fsyy.fsyyspringboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsyySpringBoot3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FsyySpringBoot3Application.class, args);
+    }
+
+}
---
+++ fsyy-spring-boot3/src/test/java/com/fsyy/fsyyspringboot3/FsyySpringBoot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsyy.fsyyspringboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsyySpringBoot3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
